abfd35a1147c2331e5b01446be3517f6716063ac
MoSwilam <muswilam@gmail.com>
2024-08-25 22:12:15 +0200

    ingress

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,6 +6,10 @@ app.use(json());
 
 const port = 3000;
 
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('Hi there!!!!!');
+});
+
 app.listen(port, () => {
   console.log(`----------- Listening on port ${port} -----------`);
 });
