8c3d626dd5bb52eb2a3addf3063e0aa23a8c9a6b
MoSwilam <muswilam@gmail.com>
2024-11-30 10:52:17 +0100

    add auth routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,15 +1,21 @@
 import express from 'express';
 import { json } from 'body-parser';
 
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { singoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+
 const app = express();
 app.use(json());
-
 const port = 3000;
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!!!!!');
-});
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(singoutRouter);
+app.use(signupRouter);
 
 app.listen(port, () => {
   console.log(`----------- Listening on port ${port} -----------`);
 });
+
